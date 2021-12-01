0edf869c7f76f5e03a0da1c0119f97ef1886614b
TonyPurple <tony.vanoni@outlook.com>
2021-12-01 15:23:48 -0800

    working on part 2 of lab

--- config/database.js
+++ config/database.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/flights', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    // useCreateIndex: true
 });
 
 // shortcut to mongoose.connection object
